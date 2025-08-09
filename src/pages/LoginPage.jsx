73134425cad53c770b362a8c5649fe3a47c6608d
R S Chiraag <chiraag.rs@gmail.com>
2025-08-09 18:38:36 +0530

    add checks to login, register pages

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, Heading, HStack, Image, Input, Link, Text, Theme, VStack } from '@chakra-ui/react';
-import { PasswordInput } from '../components/ui/password-input';
-import { FaUser } from "react-icons/fa";
+import { PasswordInput } from '../components/chakra-comps/password-input';
+import { Toaster, toaster } from '../components/chakra-comps/toaster';
 import React, { useState, useEffect } from 'react';
 
 const LoginPage = () => {
@@ -16,57 +16,68 @@ const LoginPage = () => {
         }));
     };
 
+    const validateSubmit = () => {
+        if (loginForm.password === '' || loginForm.username === '') {
+            toaster.create({
+                title: "Fill all fields",
+                type: 'error',
+                closable: true,
+            });
+            return true;
+        }
+        return false;
+    }
+
     return (
         <Box display={'flex'} justifyContent={'center'} alignItems={'center'} w={'100%'} h={'100vh'}>
             {/* Main content */}
-            {/* <Box w={'70%'} bgColor={'white'} h={'70vh'} borderRadius={'25px'}> */}
-                <HStack gap={0} w={'70%'} h={'70vh'}>
-                    {/* Left Image */}
-                    <Image 
-                        src='https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00NTUtMjAwMWEteC5qcGc.jpg' 
-                        w={'60%'}
-                        h={'70vh'}
-                        borderLeftRadius={'20px'}
-                    />
+            <HStack gap={0} w={'70%'} h={'70vh'}>
+                <Toaster />
+                {/* Left Image */}
+                <Image
+                    src='https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00NTUtMjAwMWEteC5qcGc.jpg'
+                    w={'60%'}
+                    h={'70vh'}
+                    borderLeftRadius={'20px'}
+                />
 
-                    {/* Right Form Area */}
-                    <Box 
-                        w={'40%'} 
-                        h={'70vh'} 
-                        textAlign={'center'} 
-                        display={'flex'} 
-                        justifyContent={'center'} 
-                        alignItems={'center'} 
-                        bgColor={'white'}
-                        borderRightRadius={20}
-                    >
-                        <Box w={'100%'}  px={10} color={'black'}>
-                            <Heading size={'4xl'} mb={10} fontWeight={'bold'}>Login</Heading>
-                            <Theme appearance='light'>
-                                <VStack gap={3}>
-                                    <Input 
-                                        placeholder='Enter Username' 
-                                        name={'username'} 
-                                        onChange={handleChange} 
-                                        value={loginForm.username} 
-                                    />
-                                    <PasswordInput 
-                                        placeholder='Enter Password' 
-                                        name={'password'} 
-                                        onChange={handleChange} 
-                                        value={loginForm.password} 
-                                    />
-                                    <Button>Login</Button>
-                                    <Text>
-                                        Don't have an account? {' '}
-                                        <Link href='/register' variant={'underline'} textDecoration={'underline'}>Register</Link>
-                                    </Text>
-                                </VStack>
-                            </Theme>
-                        </Box>
+                {/* Right Form Area */}
+                <Box
+                    w={'40%'}
+                    h={'70vh'}
+                    textAlign={'center'}
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    bgColor={'white'}
+                    borderRightRadius={20}
+                >
+                    <Box w={'100%'} px={10} color={'black'}>
+                        <Heading size={'4xl'} mb={10} fontWeight={'bold'}>Login</Heading>
+                        <Theme appearance='light'>
+                            <VStack gap={3}>
+                                <Input
+                                    placeholder='Enter Username'
+                                    name={'username'}
+                                    onChange={handleChange}
+                                    value={loginForm.username}
+                                />
+                                <PasswordInput
+                                    placeholder='Enter Password'
+                                    name={'password'}
+                                    onChange={handleChange}
+                                    value={loginForm.password}
+                                />
+                                <Button onClick={validateSubmit}>Login</Button>
+                                <Text>
+                                    Don't have an account? {' '}
+                                    <Link href='/register' variant={'underline'} textDecoration={'underline'}>Register</Link>
+                                </Text>
+                            </VStack>
+                        </Theme>
                     </Box>
-                </HStack>
-            {/* </Box> */}
+                </Box>
+            </HStack>
         </Box>
     )
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Heading, HStack, Image, Input, Link, Text, Theme, VStack } from '@chakra-ui/react';
-import { PasswordInput } from '../components/ui/password-input';
+import { PasswordInput } from '../components/chakra-comps/password-input';
+import { Toaster, toaster } from '../components/chakra-comps/toaster';
 import React, { useEffect, useState } from 'react';
 
 const RegisterPage = () => {
@@ -16,13 +17,34 @@ const RegisterPage = () => {
         }));
     };
 
-    useEffect(() => {
-        console.log(registerForm);
-    }, [registerForm]);
+    const isPasswordInvalid = () => {
+        if (registerForm.password !== registerForm.confirmPassword && registerForm.confirmPassword !== '' && registerForm.password !== '') {
+            toaster.create({
+                title: "Passwords do not match",
+                type: 'error',
+                closable: true,
+            });
+            return true;
+        }
+        return false;
+    };
+
+    const validateSubmit = () => {
+        if (registerForm.password === '' || registerForm.confirmPassword === '' || registerForm.username === '') {
+            toaster.create({
+                title: "Fill all fields",
+                type: 'error',
+                closable: true,
+            });
+            return true;
+        }
+        return false;
+    }
 
     return (
         <Box display={'flex'} justifyContent={'center'} alignItems={'center'} w={'100%'} h={'100vh'}>
             <HStack gap={0} w={'70%'} h={'70vh'}>
+                <Toaster />
                 {/* Left Form Area */}
                 <Box
                     w={'40%'}
@@ -38,25 +60,27 @@ const RegisterPage = () => {
                         <Heading size={'3xl'} mb={10} fontWeight={'bold'}>Register</Heading>
                         <Theme appearance='light'>
                             <VStack gap={3}>
-                                <Input 
-                                    placeholder='Enter Username' 
-                                    name={'username'} 
-                                    onChange={handleChange} 
-                                    value={registerForm.username} 
+                                <Input
+                                    placeholder='Enter Username'
+                                    name={'username'}
+                                    onChange={handleChange}
+                                    value={registerForm.username}
                                 />
-                                <PasswordInput 
-                                    placeholder='Enter Password' 
-                                    name={'password'} 
-                                    onChange={handleChange} 
-                                    value={registerForm.password} 
+                                <PasswordInput
+                                    placeholder='Enter Password'
+                                    name={'password'}
+                                    onChange={handleChange}
+                                    value={registerForm.password}
+                                    onBlur={isPasswordInvalid}
                                 />
-                                <PasswordInput 
-                                    placeholder='Confirm Password' 
+                                <PasswordInput
+                                    placeholder='Confirm Password'
                                     name={'confirmPassword'}
                                     onChange={handleChange}
                                     value={registerForm.confirmPassword}
+                                    onBlur={isPasswordInvalid}
                                 />
-                                <Button colorScheme="blue" w="100%">Register</Button>
+                                <Button colorScheme="blue" w="100%" onClick={validateSubmit}>Register</Button>
                                 <Text>
                                     Already have an account? {' '}
                                     <Link href='/login' variant={'underline'} textDecoration={'underline'}>Login</Link>
